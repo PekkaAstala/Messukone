4438790652c4987444a382404753864a2c3980c8
Pekka Astala <pekka.astala@gmail.com>
2017-04-20 21:48:10 +0300

    Changed node server to node-static

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-var http = require('http');
-
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-
-const PORT = process.env.PORT || 3000;
-
-var serve = serveStatic("./");
-
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
-});
-
-server.listen(PORT);
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
