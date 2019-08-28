a4809c2bb1f9e8860fb7be57e88f9f92d494decc
Pekka Astala <pekka.astala@gmail.com>
2019-08-28 23:33:31 +0300

    Reformatted controllers

--- js/controllers.js
+++ js/controllers.js
@@ -1,47 +1,41 @@
-'use strict';
+"use strict";
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MainCtrl', ['$scope', '$http', '$sce', function(scope, http, sce) {
-  
+angular.module("myApp.controllers", []).controller("MainCtrl", [
+  "$scope",
+  "$http",
+  "$sce",
+  function(scope, http, sce) {
     scope.updateHtml = function() {
       scope.editData.facts = sce.trustAsHtml(scope.editData.factsTinyMCE);
     };
-  
-    http.get('data.js').success(function(data)
-    {
-      scope.editData = 
-      { 
-        "name":  "Linnun nimi" ,
-        "factsTinyMCE":  "<ul> <li>FAKTA 1</li> <li>FAKTA 2</li> <li>FAKTA 3</li> <li>FAKTA 4</li> <li>FAKTA 5</li> <li>FAKTA 6</li> </ul>",
-        "facts": ""        
+
+    http.get("data.js").success(function(data) {
+      scope.editData = {
+        name: "Linnun nimi",
+        factsTinyMCE:
+          "<ul> <li>FAKTA 1</li> <li>FAKTA 2</li> <li>FAKTA 3</li> <li>FAKTA 4</li> <li>FAKTA 5</li> <li>FAKTA 6</li> </ul>",
+        facts: ""
       };
       scope.species = data;
       scope.currentBirdIndex = 0;
     });
 
-    scope.left = function()
-    {
-      if (scope.currentBirdIndex == 0)
-      {
+    scope.left = function() {
+      if (scope.currentBirdIndex == 0) {
         scope.currentBirdIndex = scope.species.length - 1;
-      }
-      else
-      {
+      } else {
         scope.currentBirdIndex--;
       }
-    }
+    };
 
-    scope.right = function()
-    {
-      if (scope.currentBirdIndex == scope.species.length - 1)
-      {
+    scope.right = function() {
+      if (scope.currentBirdIndex == scope.species.length - 1) {
         scope.currentBirdIndex = 0;
-      }
-      else
-      {
+      } else {
         scope.currentBirdIndex++;
       }
-    }
-  }]);
+    };
+  }
+]);
